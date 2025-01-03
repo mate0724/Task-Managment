625f198842667a1eb8929da7db52750f3de67e0d
Molnar Mate <molnarmate24@gmail.com>
2025-01-03 11:46:59 +0100

    Login

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+Route::get('/', function(){
+    return redirect('/login');
+});
+
+
+
 require __DIR__.'/auth.php';
